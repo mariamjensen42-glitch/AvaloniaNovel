b8655cc46e60121c9d0b857a5ee9b355a7f6a7d4
mariamjensen42-glitch <mariamjensen42@gmail.com>
2026-05-02 17:16:12 +0800

    feat: 添加PDF操作服务

---
+++ Services/PdfService.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AgentNovel.Models;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace AgentNovel.Services;
+
+public class PdfService
+{
+    public async Task<PdfFile> LoadPdfAsync(string filePath)
+    {
+        return await Task.Run(() =>
+        {
+            var document = PdfReader.Open(filePath, PdfDocumentOpenMode.Import);
+            var fileInfo = new FileInfo(filePath);
+            
+            var pdfFile = new PdfFile
+            {
+                FileName = Path.GetFileName(filePath),
+                FilePath = filePath,
+                PageCount = document.PageCount,
+                FileSize = fileInfo.Length
+            };
+
+            for (int i = 0; i < document.PageCount; i++)
+            {
+                pdfFile.Pages.Add(new PdfPage
+                {
+                    PageNumber = i + 1,
+                    Rotation = (int)document.Pages[i].Rotate
+                });
+            }
+
+            document.Close();
+            return pdfFile;
+        });
+    }
+
+    public async Task MergePdfsAsync(List<string> filePaths, string outputPath, IProgress<int>? progress = null)
+    {
+        await Task.Run(() =>
+        {
+            using var outputDocument = new PdfDocument();
+            int totalFiles = filePaths.Count;
+            int processedFiles = 0;
+
+            foreach (var filePath in filePaths)
+            {
+                using var inputDocument = PdfReader.Open(filePath, PdfDocumentOpenMode.Import);
+                foreach (var page in inputDocument.Pages)
+                {
+                    outputDocument.AddPage(page);
+                }
+                processedFiles++;
+                progress?.Report((processedFiles * 100) / totalFiles);
+            }
+
+            outputDocument.Save(outputPath);
+        });
+    }
+
+    public async Task SplitPdfAsync(string filePath, List<int> pageNumbers, string outputPath)
+    {
+        await Task.Run(() =>
+        {
+            using var inputDocument = PdfReader.Open(filePath, PdfDocumentOpenMode.Import);
+            using var outputDocument = new PdfDocument();
+
+            foreach (var pageNum in pageNumbers.OrderBy(p => p))
+            {
+                if (pageNum >= 1 && pageNum <= inputDocument.PageCount)
+                {
+                    outputDocument.AddPage(inputDocument.Pages[pageNum - 1]);
+                }
+            }
+
+            outputDocument.Save(outputPath);
+        });
+    }
+
+    public async Task ReorderPagesAsync(string filePath, List<int> newOrder, string outputPath)
+    {
+        await Task.Run(() =>
+        {
+            using var inputDocument = PdfReader.Open(filePath, PdfDocumentOpenMode.Import);
+            using var outputDocument = new PdfDocument();
+
+            foreach (var pageNum in newOrder)
+            {
+                if (pageNum >= 1 && pageNum <= inputDocument.PageCount)
+                {
+                    outputDocument.AddPage(inputDocument.Pages[pageNum - 1]);
+                }
+            }
+
+            outputDocument.Save(outputPath);
+        });
+    }
+
+    public async Task RotatePagesAsync(string filePath, List<int> pageNumbers, int rotation, string outputPath)
+    {
+        await Task.Run(() =>
+        {
+            using var inputDocument = PdfReader.Open(filePath, PdfDocumentOpenMode.Import);
+            
+            foreach (var pageNum in pageNumbers)
+            {
+                if (pageNum >= 1 && pageNum <= inputDocument.PageCount)
+                {
+                    var page = inputDocument.Pages[pageNum - 1];
+                    page.Rotate = (PdfPageRotate)(((int)page.Rotate + rotation) % 360);
+                }
+            }
+
+            inputDocument.Save(outputPath);
+        });
+    }
+
+    public async Task DeletePagesAsync(string filePath, List<int> pageNumbers, string outputPath)
+    {
+        await Task.Run(() =>
+        {
+            using var inputDocument = PdfReader.Open(filePath, PdfDocumentOpenMode.Import);
+            var pagesToKeep = Enumerable.Range(1, inputDocument.PageCount)
+                .Where(p => !pageNumbers.Contains(p))
+                .ToList();
+
+            using var outputDocument = new PdfDocument();
+            foreach (var pageNum in pagesToKeep)
+            {
+                outputDocument.AddPage(inputDocument.Pages[pageNum - 1]);
+            }
+
+            outputDocument.Save(outputPath);
+        });
+    }
+}
